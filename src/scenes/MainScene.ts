ac1e8c9de2aa13e8e702cfd3c5c23b9e80263994
Marcin228 <marcin@goingforit.pl>
2024-06-03 19:01:02 +0200

    Add onjump effect.

--- src/scenes/MainScene.ts
+++ src/scenes/MainScene.ts
@@ -7,6 +7,7 @@ export default class MainScene extends Scene{
     private backgroundIdx2:Phaser.GameObjects.TileSprite;
     private backgroundIdx3:Phaser.GameObjects.TileSprite;
 
+    private splash:Phaser.GameObjects.Sprite;
     private runner:Phaser.Physics.Arcade.Sprite;
     private isJumping:boolean;
 
@@ -22,6 +23,7 @@ export default class MainScene extends Scene{
         this.load.image('backgroundIdx3', 'assets/img/backgroundIdx3.png');
 
         this.load.spritesheet('runner', 'assets/img/run.png', { frameWidth: 512, frameHeight: 512 });
+        this.load.spritesheet('splash', 'assets/img/splash.png', { frameWidth: 512, frameHeight: 512 });
     }
 
     create(){
@@ -36,6 +38,13 @@ export default class MainScene extends Scene{
         this.backgroundIdx1 = this.add.tileSprite(960, 540, 1920, 1080, 'backgroundIdx1').setDisplaySize(screen.width, screen.height);
 
         this.runner = this.physics.add.sprite(100, 100, 'runner').setScale(0.2);
+        this.splash = this.add.sprite(this.runner.x, 0, 'splash').setScale(0.2);
+        this.splash.visible = false;
+        this.splash.on('animationcomplete', (animation:any, frame:any) => {
+            if(animation.key === 'splash')
+                this.splash.visible = false;
+        }, this);
+
         this.isJumping = false;
         this.runner.setCollideWorldBounds(true);
 
@@ -46,6 +55,13 @@ export default class MainScene extends Scene{
             repeat: -1
         });
 
+        this.anims.create({
+            key:'splash',
+            frames: this.anims.generateFrameNumbers('splash', {start:0, end: 3}),
+            frameRate: 10,
+            repeat: 0
+        });
+
         this.input.keyboard?.on('keydown-SPACE', this.makeRunnerJump, this);
 
         this.runner.anims.play('run', true);
@@ -54,6 +70,10 @@ export default class MainScene extends Scene{
     makeRunnerJump(){
 
         if(!this.isJumping){
+
+            this.splash.visible = true;
+            this.splash.anims.play('splash', true);
+
             this.isJumping = true;
             this.runner.setVelocityY(-300);
 
@@ -70,8 +90,11 @@ export default class MainScene extends Scene{
         this.backgroundIdx2.tilePositionX += 4;
         this.backgroundIdx3.tilePositionX += 1;
 
-        if(this.runner?.body?.blocked.down)
+        if(this.runner?.body?.blocked.down){
             this.isJumping = false;
+            this.splash.x = this.runner.x;
+            this.splash.y = this.runner.y;
+        }
         
         if(!this.isJumping)
             this.runner.anims.currentAnim?.resume();
